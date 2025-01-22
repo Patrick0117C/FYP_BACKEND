3f615abe517b3489965abbed3859e7a28d581061
Cheng Chi Pang <98073329+Patrick0117C@users.noreply.github.com>
2025-01-22 15:15:18 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://patrickcheng0117:h6SxcOm2TkAsbsme@fypcluster.skpcx.mongodb.net/?retryWrites=true&w=majority&appName=FYPCluster").then(() => {
+mongoose.connect("mongodb+srv://patrickcheng0117:h6SxcOm2TkAsbsme@fypcluster.skpcx.mongodb.net/FYP?retryWrites=true&w=majority&appName=FYPCluster").then(() => {
     console.log('Connected to MongoDB');
 }).catch(err => {
     console.log(err);
